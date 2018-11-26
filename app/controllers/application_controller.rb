86eab9d9119dd5d54291c6327b7b0ff903571a77
Derrick <dbozich1@gmail.com>
2018-11-25 17:01:33 -0700

    added devise username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+   devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
   # def current_user
   #   @current_user ||= User.find_by_id(session[:user_id])
   # end
@@ -8,4 +15,6 @@ class ApplicationController < ActionController::Base
   #   !!current_user
   # end
   helper_method :logged_in?
+
+
 end
